388508a547c71542bc96dff7513ef52e96b916aa
Hugues <hugues.poncet@yahoo.fr>
2024-11-06 09:24:24 +0100

    Seconde fenetre de la modale créé, à terminer

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,48 +1,24 @@
+
 const openModal = function (e) {
     e.preventDefault()
     const target = document.querySelector(e.currentTarget.getAttribute('href'))
     if (target) {
-        target.style.display = null;
+        target.style.display = null
         target.removeAttribute('aria-hidden')
         target.setAttribute('aria-modal', 'true')
     }
 }
 
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json())
-    .then(data => {
-        afficherModalGalerie(data);
-        return data;
-    })
-    .catch(error => console.error('Erreur:', error))
 
-
-function afficherModalGalerie(works) {
-    const galerie = document.getElementById('modal-galerie');
-    galerie.innerHTML = '';
-
-    works.forEach(work => {
-        const figure = document.createElement('figure');
-        const imgElement = document.createElement('img');
-        imgElement.src = work.imageUrl;
-        imgElement.alt = work.title || 'Image';
-        const figcaption = document.createElement('figcaption');
-
-        figure.appendChild(imgElement);
-        figure.appendChild(figcaption);
-        galerie.appendChild(figure);
-    });
-}
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+})
 
 const closeModal = function (modal) {
     modal.style.display = "none"
     modal.setAttribute('aria-hidden', 'true')
     modal.removeAttribute('aria-modal')
 }
-
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal)
-})
 
 document.querySelectorAll('.modal').forEach(modal => {
     modal.addEventListener('click', function (e) {
@@ -57,6 +33,50 @@ document.querySelectorAll('.modal').forEach(modal => {
             closeModal(modal)
         })
     }
+
+    const page1 = modal.querySelector('.modal-page1')
+    const page2 = modal.querySelector('.modal-page2')
+    const returnButton = modal.querySelector('.retour')
+
+    page2.classList.add('hidden')
+
+
+    const nextPageButton = modal.querySelector('.ajout-photo')
+    if (nextPageButton) {
+        nextPageButton.addEventListener('click', function () {
+            page1.classList.add('hidden')
+            page2.classList.remove('hidden')
+        })
+    }
+
+    returnButton.addEventListener('click', function () {
+        page2.classList.add('hidden')
+        page1.classList.remove('hidden')
+        afficherModalGalerie()
+    })
+
 })
 
+function afficherModalGalerie() {
+    const galerie = document.getElementById('modal-galerie')
+    galerie.innerHTML = ''
 
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then(works => {
+            works.forEach(work => {
+                const figure = document.createElement('figure')
+                const imgElement = document.createElement('img')
+                imgElement.src = work.imageUrl
+                imgElement.alt = work.title || 'Image'
+                const figcaption = document.createElement('figcaption')
+
+                figure.appendChild(imgElement)
+                figure.appendChild(figcaption)
+                galerie.appendChild(figure)
+            })
+        })
+        .catch(error => console.error('Erreur:', error))
+}
+
+afficherModalGalerie()
