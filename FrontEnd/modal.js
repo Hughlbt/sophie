05cc1be12bddd75b78cee9952b2085df5d37f09f
Hugues <hugues.poncet@yahoo.fr>
2024-11-11 10:04:57 +0100

    Le bouton de suppression est fonctionelle, hormis le style, mais il me renvoi actuellement l'erreur 401

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -33,7 +33,7 @@ document.querySelectorAll('.modal').forEach(modal => {
             closeModal(modal);
         });
     });
-    
+
 
     const page1 = modal.querySelector('.modal-page1')
     const page2 = modal.querySelector('.modal-page2')
@@ -53,30 +53,57 @@ document.querySelectorAll('.modal').forEach(modal => {
     returnButton.addEventListener('click', function () {
         page2.classList.add('hidden')
         page1.classList.remove('hidden')
-        afficherModalGalerie()
+
     })
 
 })
 
 function afficherModalGalerie() {
     const galerie = document.getElementById('modal-galerie')
-    galerie.innerHTML = ''
 
     fetch('http://localhost:5678/api/works')
         .then(response => response.json())
         .then(works => {
             works.forEach(work => {
-                const figure = document.createElement('figure')
+                const figure = document.createElement('figure') //figure = <figure></figure>
                 const imgElement = document.createElement('img')
+                const deleteButton = document.createElement('button')
                 imgElement.src = work.imageUrl
                 imgElement.alt = work.title || 'Image'
                 const figcaption = document.createElement('figcaption')
                 figure.appendChild(imgElement)
                 figure.appendChild(figcaption)
                 galerie.appendChild(figure)
+                figure.appendChild(deleteButton)
+
+                deleteButton.textContent = 'Delete'
+                deleteButton.addEventListener('click', () => {
+                   deleteWork(work.id)
+                   figure.remove
+                })
+
             })
         })
         .catch(error => console.error('Erreur:', error))
+
+    //Recupérer tous les boutons faire une boucle et utiliser le addeventlistener dessus//
 }
+
+function deleteWork(workId) {
+    fetch(`http://localhost:5678/api/works/${workId}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log(`Image avec id ${workId} supprimée`)
+
+            } else {
+                console.error('Erreur lors de la suppression')
+            }
+        })
+        .catch(error => console.error('Erreur:', error))
+}
+
+
 
 afficherModalGalerie()
